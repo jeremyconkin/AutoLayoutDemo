e0b989fc7dba2f322192a16926fb39a58290a9f8
Jeremy Conkin <jconkin@gmail.com>
2017-01-26 14:16:47 -0700

    Info text, position page, scale page, and animation page.

--- AutolayoutDemo/AnimationViewController.swift
+++ AutolayoutDemo/AnimationViewController.swift
@@ -10,4 +10,27 @@ import UIKit
 
 class AnimationViewController: AutolayoutExampleViewController {
 
+    @IBOutlet weak var topConstraint: NSLayoutConstraint!
+
+    override func viewWillAppear(_ animated: Bool) {
+
+        super.viewWillAppear(animated)
+        topConstraint.constant = 0
+        view.layoutIfNeeded()
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+
+        super.viewDidAppear(animated)
+
+        UIView.animate(withDuration: 1.0,
+                       delay: 0,
+                       options: UIViewAnimationOptions(),
+                       animations: {
+                        self.topConstraint.constant = 300
+                        self.view.layoutIfNeeded()
+        },
+                       completion: nil)
+    }
+
 }
